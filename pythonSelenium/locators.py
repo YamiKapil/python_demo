0e418dc1dbf2d69e424442d780366497faef61d9
YamiKapil <yamikapil@gmail.com>
2024-06-23 17:50:29 +0545

    locator done

--- pythonSelenium/locators.py
+++ pythonSelenium/locators.py
@@ -21,3 +21,23 @@ driver.find_element(by=By.XPATH, value="//input[@type='submit']").click()
 successText = driver.find_element(by=By.CLASS_NAME, value="alert-success").text
 print(successText)
 
+# sales force login..
+driver.get("https://login.salesforce.com")
+
+# generating css from id and class
+# tagName#id eg: input#username or #username (only for id)
+# tagName.className eg: input.password or .password (only for class)
+driver.find_element(by=By.CSS_SELECTOR, value="#username").send_keys("UserName")
+driver.find_element(by=By.CSS_SELECTOR, value=".password").send_keys("Pass")
+# to clear any data from the field
+driver.find_element(by=By.CSS_SELECTOR, value=".password").clear()
+
+# link text the element should start wht <a> tag
+driver.find_element(by=By.LINK_TEXT, value="Forgot Your Password?").click()
+# getting the element with text using xPath
+# driver.find_element(by=By.XPATH, value="//a[text()='Cancel']").click()
+
+
+
+
+
